adcf421cc173c9316c676339154daa8cd662a9b1
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-11-28 19:17:25 +0530

    convention fix

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ecomSlice from "../features/ecomSlice/ecomSlice";
-import authSlice from "../features/authSlice/AuthSlice";
+import authSlice from "../features/authSlice/authSlice";
 
 const store = configureStore({
   reducer: { ecom: ecomSlice.reducer, auth: authSlice.reducer },
